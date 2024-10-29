443536f1328b4a6191197c39b862bd3584900a95
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-29 13:43:07 -0400

    Add files via upload

---
+++ 002_cos/cos/math/Math.py
@@ -0,0 +1,18 @@
+# Math.py
+
+from .divide import divide
+from .multiply import multiply
+
+class Math:
+    # assigning functions as static methods
+    divide = staticmethod(divide)
+    multiply = staticmethod(multiply)
+
+##
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
---
+++ usesOurPackage.py
@@ -0,0 +1,21 @@
+# usesOurPackage.py
+
+from cos import Math
+
+# using as class attributes
+print(Math.divide(10, 2))
+print(Math.multiply(3, 4))
+
+# using an instance
+math_instance = Math()
+print(math_instance.divide(10, 2))
+print(math_instance.multiply(3, 4))
+
+##
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
